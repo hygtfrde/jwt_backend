98f9ae0f4f3edd8cff22095b114d6897283d2b83
harold <haroldulrich@protonmail.com>
2023-07-13 00:56:30 -0700

    bearer token

--- middleware/authRequired.js
+++ middleware/authRequired.js
@@ -1,19 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
- console.log('init authRequired');
+  console.log('init authRequired');
 
- let bearerHeader = req.headers["authorization"];
- console.log("bearerHeader ---> ", bearerHeader);
+  let bearerHeader = req.headers["authorization"];
+  console.log("bearerHeader ---> ", bearerHeader);
 
- if (bearerHeader) {
+  if (bearerHeader) {
     const bearerToken = bearerHeader.split(" ")[1];
-    let verified = jwt.verify(bearerToken, "waffles");
-    console.log("Here's the verified token", verified);
-    req.userId = verified._id;
-    next();
-  }
-  else {
-    res.status(403).send("Middlewar broke. No bearer token.");
+    try {
+      let verified = jwt.verify(bearerToken, "waffles");
+      console.log("Here's the verified token", verified);
+      req.userId = verified._id;
+      next();
+    } catch (error) {
+      res.status(401).send("Bearer token unauthorized.");
+    }
+  } else {
+    res.status(403).send("Bearer token missing.");
   }
 };
